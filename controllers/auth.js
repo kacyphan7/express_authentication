65e68dd22ac9ac9adc003df4fdead0246c6c5060
Kacy Phan <kacyphan@Kacys-Mac-mini.lan>
2023-05-24 21:22:03 -0700

    feat: [auth] add logout get route

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,12 +13,23 @@ router.get("/login", (req, res) => {
   return res.render("auth/login");
 });
 
+router.get('/logout', (req, res) => {
+  req.logOut(function (err, next) {
+    if (err) {
+      return next(err);
+    }
+    req.flash('success', 'Logging out... See you next time!');
+    res.redirect('/');
+  }); // logs the user out of the session
+});
+
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/auth/login',
   successFlash: 'Welcome back ...',
   failureFlash: 'Either email or password is incorrect'
 }));
+
 
 router.post('/signup', async (req, res) => {
   // we now have access to the user info (req.body);
